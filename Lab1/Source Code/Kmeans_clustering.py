7da60cb0626a619c9984bd4a7d91e585cb606a7f
htata31 <htata31@gmail.com>
2019-07-01 15:53:07 -0500

    Commit3

--- Lab1/Source Code/Kmeans_clustering.py
+++ Lab1/Source Code/Kmeans_clustering.py
@@ -8,65 +8,69 @@ sns.set(style="white", color_codes=True)
 import warnings
 warnings.filterwarnings("ignore")
 
+# Import the data into a dataframe
 data = pd.read_csv(Path('Datasets/College.csv'))
+# To check the number of values in the private column
 print(data["Private"].value_counts())
-
+# check for the null values if they are present are not
 nulls = pd.DataFrame(data.isnull().sum().sort_values(ascending=False)[:25])
 nulls.columns  = ['Null Count']
 nulls.index.name  = 'Feature'
 print(nulls)
 
 # There are no null counts in the data set so no need to take mean of the particular features
+
+# Divide the dependent and independent data 
 y = data.iloc[:, 1:2]
 x = data.iloc[:, 2:]
 print(x.shape, y.shape)
 
+# Elbow method is used for finding the optimal nnumber of clusters
 wcss = []
-for i in range(1, 7):
+for i in range(1, 4):
     kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
     kmeans.fit(x)
     wcss.append(kmeans.inertia_)
 # print(wcss)
-# plt.plot(range(1,7),wcss)
-# plt.title('The Elbow Graph')
-# plt.xlabel('Number of Clusters')
-# plt.ylabel('Wcss')
-# plt.show()
+plt.plot(range(1,4),wcss)
+plt.title('The Elbow Graph')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Wcss')
+plt.show()
 
-# nclusters = 3 # this is the k in kmeans
-km = KMeans(n_clusters=3)
+# Apply k mean clustering with no of clusters = 2
+km = KMeans(n_clusters=2)
 km.fit(x)
 y_cluster_kmeans = km.predict(x)
 from sklearn import metrics
 score = metrics.silhouette_score(x, y_cluster_kmeans)
-print("Accuracy score is: ", score)
+print("The silhouette_score is: ", score)
 
+plt.scatter(x.iloc[:,10],x.iloc[:,5], c=y_cluster_kmeans[:,], s=30, cmap='viridis')
 
+# Standardiazation is not required as the data is normalized and the accuracy score is falling down.
+# standardization
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+# Fit on training set only.
+scaler.fit(x)
+# Apply transform to both the training set and the test set.
+x_scaler = scaler.transform(x)
 
-# Standardiazation is not required as the data is normalized
-# # standardization
-# from sklearn.preprocessing import StandardScaler
-# scaler = StandardScaler()
-# # Fit on training set only.
-# scaler.fit(x)
-# # Apply transform to both the training set and the test set.
-# x_scaler = scaler.transform(x)
-
-
+# Apply PCA on the data with dimension reduction to 2 axis
 pca = PCA(2)
 x_pca = pca.fit_transform(x)
 df2 = pd.DataFrame(data=x_pca)
 finaldf = pd.concat([df2, data[['Private']]], axis=1)
 print(finaldf)
 
-# KMeans after standarization
-km = KMeans(n_clusters=3)
+# KMeans after PCA
+km = KMeans(n_clusters=2)
 km.fit(x_pca)
 y_cluster_kmeans= km.predict(x_pca)
 from sklearn import metrics
 score = metrics.silhouette_score(x_pca, y_cluster_kmeans)
-print(score)
+print("The silhouette_score after PCA is",score)
 
-plt.scatter(x.iloc[:, 0], x.iloc[:, 1], c=y_cluster_kmeans, s=50, cmap='viridis')
-centers = kmeans.cluster_centers_
-# plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
+# Plotting the clusters 
+plt.scatter(x.iloc[:,10],x.iloc[:,5], c=y_cluster_kmeans[:,], s=30, cmap='viridis')
